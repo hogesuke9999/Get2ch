a6004de154423b89066770efca7b2ca23a893c52
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-18 22:25:31 +0900

    認証用ページの更新

--- htdocs/ident.pl
+++ htdocs/ident.pl
@@ -21,13 +21,13 @@ print $cgi->header('text/html' -charset => "utf-8",);
 # start the HTML
 print $cgi->start_html(-title => '2ちゃんねる スレッド一覧', -lang => 'ja', -encoding => 'utf-8');
 
-print "<form action=\"ident.pl\" method=\"post\">";
+print "<form action=\"ident.pl\" method=\"post\">\n";
 print "<table>\n";
 print "<tr>\n";
-print "<td>" . "User Name : " . "</td>" . "<tr>" . "<input type=\"text\" name=\"user_name\" size=\"16\">" . "</tr>\n";
+print "<td>" . "User Name : " . "</td>" . "<td>" . "<input type=\"text\" name=\"user_name\" size=\"16\">" . "</td>\n";
 print "</tr>\n";
 print "<tr>\n";
-print "<td>" . "Password  : " . "</td>" . "<tr>" . "<input type=\"text\" name=\"user_pass\" size=\"16\">" . "</tr>\n";
+print "<td>" . "Password  : " . "</td>" . "<td>" . "<input type=\"text\" name=\"user_pass\" size=\"16\">" . "</td>\n";
 print "</tr>\n";
 print "</table>\n";
 print "<input type=\"submit\" value=\"送信\">\n";
