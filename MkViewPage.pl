c29fd96e2e348095d085f01a6e27ed840f45dbe5
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-18 11:47:07 +0900

    更新

--- MkViewPage.pl
+++ MkViewPage.pl
@@ -1,3 +1,19 @@
 #!/usr/bin/perl
 
+# load CGI routines
 use CGI;
+
+# create new CGI object
+my $q = CGI->new;
+
+# create the HTTP header
+print $q->header();
+
+# start the HTML
+print $q->start_html('hello world');
+
+# level 1 header
+print $q->h1('hello world');
+
+# end the HTML
+print $q->end_html;
