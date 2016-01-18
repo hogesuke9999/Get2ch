a5cc9a8e9c9bc24479c279ea253279e28e5d42ab
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-18 22:05:53 +0900

    認証用ページ

---
+++ htdocs/ident.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+# load CGI routines
+use CGI;
+
+# create new CGI object
+my $cgi = CGI->new;
+
+print $cgi->charset("utf-8");
+
+# create the HTTP header
+print $cgi->header('text/html' -charset => "utf-8",);
+
+# start the HTML
+print $cgi->start_html(-title => '2ちゃんねる スレッド一覧', -lang => 'ja', -encoding => 'utf-8');
+
+# end the HTML
+print $cgi->end_html;
