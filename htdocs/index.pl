14cad8ca4013650e61e6af90a55fb5d7e4510c78
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-20 01:08:56 +0900

    認証を含むスレッド一覧表示用ページの更新

--- htdocs/index.pl
+++ htdocs/index.pl
@@ -23,20 +23,16 @@ print $cgi->header(
 # start the HTML
 print $cgi->start_html(-title => '2ちゃんねる スレッド一覧', -lang => 'ja', -encoding => 'utf-8');
 
-$button_name = $cgi->param('button');
-# print "ボタン : " . $button_name . "<br>\n";
-
 print $cgi->start_form("post", "index.pl");
 
 # level 1 header
 print $cgi->h1('2ちゃんねる スレッド一覧');
 
+$button_name = $cgi->param('button');
 if( $button_name eq "送信") {
         $user_name = $cgi->param('user_name');
         $user_pass = $cgi->param('user_pass');
 
-#        print "User Name     : " . $user_name . "<br>\n";
-#        print "User Password : " . $user_pass . "<br>\n";
         if($user_name eq $user_pass){
                 $cgi_session->param("user_name", $user_name);
                 $login_flag = 1;
@@ -44,12 +40,12 @@ if( $button_name eq "送信") {
 }
 
 if( $button_name eq "ログアウト") {
-        print "ログアウトしました<br>\n";
+        print "ログアウトしました" .  $cgi->br . "\n";
         $cgi_session->delete();
 } else {
         $user_name = $cgi_session->param("user_name");
         if($user_name ne "") {
-                print "User Name : " . $user_name . "<br>\n";
+                print "User Name : " . $user_name . "\n";
                 print $cgi->submit('button', 'ログアウト');
                 $login_flag = 1;
         }
@@ -75,55 +71,11 @@ if($login_flag == 0) {
 
 print $cgi->end_form;
 
-print "<hr>\n";
-print "<a href=\"index.pl\">再表示</a>\n";
+print $cgi->hr . "\n";
+print $cgi->a({href=>"index.pl"}, "再表示");
+# print "<a href=\"index.pl\">再表示</a>\n";
 
 # end the HTML
 print $cgi->end_html;
 
 exit;
-
-
-
-
-
-my $user_name = $cgi_session->param("user_name");
-if($user_name eq "") {
-#	print "セッションに登録されていません<br>\n";
-	$user_name = $cgi->param('user_name');
-	$user_pass = $cgi->param('user_pass');
-
-	if($user_name eq "") {
-		print "認証が行われていません<br>\n";
-	#	print "<form action=\"ident.pl\" method=\"post\">\n";
-		print "<table>\n";
-		print "<tr>\n";
-		print "<td>" . "User Name : " . "</td>\n";
-		print "<td>";
-		print $cgi->textfield('user_name','',16,16);
-	#	print "<input type=\"text\" name=\"user_name\" size=\"16\">";
-		print "</td>\n";
-		print "</tr>\n";
-		print "<tr>\n";
-		print "<td>" . "Password  : " . "</td>\n";
-		print "<td>";
-		print $cgi->textfield('user_pass','',16,16);
-	#	print "<input type=\"text\" name=\"user_pass\" size=\"16\">";
-		print "</td>\n";
-		print "</tr>\n";
-		print "</table>\n";
-	} else {
-#		print "認証が行われています<br>\n";
-		print "User Name     : " . $user_name . "<br>\n";
-		print "User Password : " . $user_pass . "<br>\n";
-		if($user_name eq $user_pass){
-#			print "セッションに登録します<br>\n";
-			$cgi_session->param("user_name", $user_name);
-#			$cgi_session->flush();
-		}
-	}
-} else {
-#	print "セッションに登録されています<br>\n";
-        print "認証済み<br>\n";
-	print "User Name : " . $user_name . "<br>\n";
-}
