a37f5b88595f5800a8a76244b6ed2130e89a773f
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-22 09:32:50 +0900

    スレッドの読み込み 更新

--- GetThread/GetThread.pl
+++ GetThread/GetThread.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 
 use LWP::UserAgent;
+use Encode;
+use CGI;
 
 my $ua = LWP::UserAgent->new;
 $ua->timeout(10);
@@ -8,6 +10,17 @@ $ua->env_proxy;
 $ua->agent("");
 
 my $URL = "http://daily.2ch.net/test/read.cgi/newsplus/1453205001/-3";
+
+# create new CGI object
+my $cgi = CGI->new;
+
+print $cgi->charset("utf-8");
+
+# create the HTTP header
+print $cgi->header('text/html' -charset => "utf-8",);
+
+# start the HTML
+print $cgi->start_html(-title => '2ちゃんねる スレッド', -lang => 'ja', -encoding => 'utf-8');
 
 my $response = $ua->get($URL);
 
@@ -24,3 +37,7 @@ if ($response->is_success) {
 } else {
 	die $response->status_line;
 }
+
+
+# end the HTML
+print $cgi->end_html;
