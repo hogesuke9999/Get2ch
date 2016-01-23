23f7341cad41a5e74db7bd94eb6287a71cb90a74
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-24 01:07:18 +0900

    スレッド一覧のDBへの登録

--- GetThread.pl
+++ GetThread.pl
@@ -9,20 +9,13 @@ use DBI;
 
 my $ConfFileName = $ARGV[0];
 if($ConfFileName eq "") {
-	print "Get2chThread.pl Get2chThread.conf\n";
+	print "GetThread.pl Get2chThread.conf\n";
 	exit 1;
 }
 
 # データベース接続設定の読み込み
 our $DB_NAME, $DB_USER, $DB_PASS, $DB_HOST, $DB_PORT;
 require $ConfFileName;
-
-# print "DB_NAME = " . $DB_NAME . "\n";
-# print "DB_USER = " . $DB_USER . "\n";
-# print "DB_PASS = " . $DB_PASS . "\n";
-# print "DB_HOST = " . $DB_HOST . "\n";
-# print "DB_PORT = " . $DB_PORT . "\n";
-
 
 # データベース接続用ハンドルを取得
 my $db = DBI->connect("dbi:Pg:dbname=$DB_NAME;host=$DB_HOST;port=$DB_PORT", "$DB_USER","$DB_PASS")
@@ -33,55 +26,38 @@ $ua->timeout(10);
 $ua->env_proxy;
 $ua->agent("");
 
-# my $ThreadList = require $ThreadListFile;
-# print $ThreadList -> {'newsplus'}{'threadhost'} . "\n";
-# print $ThreadList -> {'newsplus'}{'threadname'} . "\n";
+$sql_board = "select name, host, title from board;";
+my $sth_board = $db->prepare($sql_board);
+$sth_board->execute;
 
-$sql = "select name, host, title from board;";
-my $sth = $db->prepare($sql);
-$sth->execute;
-
-while (my $arr_ref = $sth->fetchrow_arrayref) {
-	my ($board_name, $board_host, $board_title) = @$arr_ref;
+while (my $board_arr_ref = $sth_board->fetchrow_arrayref) {
+	my ($board_name, $board_host, $board_title) = @$$board_arr_ref;
 	print "Name  : " . $board_name  . "\n";
 	print "Host  : " . $board_host  . "\n";
 	print "Title : " . $board_title . "\n";
-}
-$sth->finish;
 
-$db->disconnect;
-
-exit;
-
-for my $ThreadList_tag (sort keys %$ThreadList) {
-	print "ThreadName = " . $ThreadList -> {$ThreadList_tag}{'threadname'} . "\n";
-	print "ThreadHost = " . $ThreadList -> {$ThreadList_tag}{'threadhost'} . "\n";
-	print "\n";
-
-	my $response = $ua->get($ThreadList -> {$ThreadList_tag}{'threadhost'} . "/" . $ThreadList_tag . "/subback.html");
+	my $response = $ua->get($board_host . $board_name . "/subback.html");
 
 	if ($response->is_success) {
 		my @page = split( '\n', $response->content );
 		my $cnt = 1;
 		foreach my $line ( @page ) {
-			# <a href="1452481229/l50">855: 【社会】震災４年１０か月 ボランティアが不明者捜索 [無断転載禁止]&#169;2ch.net (15)</a>
 			if ( $line =~ '^<a href=.*</a>$' ) {
-				my ( $id, $title ) = ( $line =~ /<a href="(\d+)\/.*: (.*)\(.\d*\)<\/a>/) ;
-				my $title_utf8 = encode('utf-8', decode('sjis', $title));
+				my ( $thread_id, $thread_title ) = ( $line =~ /<a href="(\d+)\/.*: (.*)\(.\d*\)<\/a>/) ;
+				my $thread_title_utf8 = encode('utf-8', decode('sjis', $thread_title));
 
-				my $sql = "select count(*) from subjects where id = '" . $id . "';";
+				my $sql = "select count(*) from threads where id = '" . $thread_id . "';";
 				my $sth = $db->prepare($sql);
 				$sth->execute;
 
 				my $arr_ref = $sth->fetchrow_arrayref;
-				my ($id_exist) = @$arr_ref;
+				my ($thread_id_exist) = @$arr_ref;
 				$sth->finish;
 
-				if ( $id_exist == 0 ) {
-					print $cnt . " : " . $id . "(" . $id_exist . ") = " . $title_utf8 . "\n";
-					my $sql = "insert into subjects (id, tag, datetime, subject) values ('" . $id . "', '" . $ThreadList_tag . "', now(), '" . $title_utf8 . "');";
-# print "SQL = " . $sql . "\n";
-					$db->do($sql);
+				if ( $thread_id_exist == 0 ) {
+					print $cnt . " : " . $thread_id . "(" . $thread_id_exist . ") = " . $thread_title_utf8 . "\n";
+					my $sql = "insert into threads (id, board_name, createtime, title) values ('" . $thread_id . "', '" . $board_name . "', now(), '" . $thread_title_utf8 . "');";
+#					$db->do($sql);
 				}
 			}
 			$cnt = $cnt + 1;
@@ -90,5 +66,8 @@ for my $ThreadList_tag (sort keys %$ThreadList) {
 		die $response->status_line;
 	}
 };
+
+}
+$sth->finish;
 
 $db->disconnect;
