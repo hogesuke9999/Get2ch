754178a67453dbe7236d818365d1ccf1c72c7ae7
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-25 22:56:51 +0900

    Tableサイズ・Windowサイズ

--- htdocs/tablesize.pl
+++ htdocs/tablesize.pl
@@ -8,24 +8,44 @@ my $cgi = CGI->new;
 $cgi->charset("utf-8");
 
 # create the HTTP header
-print $cgi->header( -type => 'text/html', -charset => 'utf-8');
+print $cgi->header(
+	-type => 'text/html',
+	-charset => 'utf-8'
+);
 
 # start the HTML
-print $cgi->start_html(-title => 'Tableサイズ・Windowサイズ', -lang => 'ja', -encoding => 'utf-8');
+print $cgi->start_html(
+	-title => 'Tableサイズ・Windowサイズ',
+	-lang => 'ja',
+	-encoding => 'utf-8',
+	-script => {
+		-type => 'JAVASCRIPT',
+		-code  => '
+			getWindowSize();
+			function getWindowSize() {
+				var sW,sH,s;
+				sW = win dow.innerWidth;
+				sH = window.innerHeight;
+				s = \"横幅 = \" + sW + \" / 高さ = \" + sH;
+				document.getElementById(\"WinSize\").innerHTML = s;
+			};
+		'
+	}
+);
 
 print $cgi->h1('Tableサイズ・Windowサイズ');
 print "\n";
 print "<p>ウィンドウサイズ：<span id=\"WinSize\"></span></p>\n";
-print "<script type=\"text/javascript\">\n";
-print "getWindowSize();\n";
-print "function getWindowSize() {\n";
-print "	var sW,sH,s;\n";
-print "	sW = window.innerWidth;\n";
-print "	sH = window.innerHeight;\n";
-print "	s = \"横幅 = \" + sW + \" / 高さ = \" + sH;\n";
-print "	document.getElementById(\"WinSize\").innerHTML = s;\n";
-print "}\n";
-print "</script>\n";
+# print "<script type=\"text/javascript\">\n";
+# print "getWindowSize();\n";
+# print "function getWindowSize() {\n";
+# print "	var sW,sH,s;\n";
+# print "	sW = window.innerWidth;\n";
+# print "	sH = window.innerHeight;\n";
+# print "	s = \"横幅 = \" + sW + \" / 高さ = \" + sH;\n";
+# print "	document.getElementById(\"WinSize\").innerHTML = s;\n";
+# print "}\n";
+# print "</script>\n";
 
 # end the HTML
 print $cgi->end_html;
