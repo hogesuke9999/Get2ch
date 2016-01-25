e0636e805aa29421f836d61737175ba94142e28b
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-25 22:32:33 +0900

    Tableサイズ・Windowサイズ

--- htdocs/tablesize.pl
+++ htdocs/tablesize.pl
@@ -15,5 +15,16 @@ print $cgi->start_html(-title => 'Tableサイズ・Windowサイズ', -lang => 'j
 
 print $cgi->h1('Tableサイズ・Windowサイズ');
 
+print "<script type=\"text/javascript\">\n";
+print "getWindowSize();\n";
+print "function getWindowSize() {\n";
+print "	var sW,sH,s;\n";
+print "	sW = window.innerWidth;\n";
+print "	sH = window.innerHeight;\n";
+print "	s = \"横幅 = \" + sW + \" / 高さ = \" + sH;\n";
+print "	document.getElementById(\"WinSize\").innerHTML = s;\n";
+print "}\n";
+print "</script>\n";
+
 # end the HTML
 print $cgi->end_html;
