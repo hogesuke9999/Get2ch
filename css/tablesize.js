ac5c356ddf9b647946fc1bbb4f671b1410d92e7b
Hogesuke9999 <keisuke@e-nyanko.net>
2016-02-07 22:35:33 +0900

    CSSとJavascriptの試験_002

--- css/tablesize.js
+++ css/tablesize.js
@@ -4,9 +4,31 @@ function getWindowSize() {
 	sH = window.innerHeight;
 	s = "横幅 = " + sW + " / 高さ = " + sH;
         document.write(s);
+	document.write("<br>");
 	if(sW > sH) {
 		document.write("横長です");
 	} else {
 		document.write("縦長です");
-	}
+	};
+	document.write(getAllStyleRule());
 };
+
+function getAllStyleRule() {
+	var styleSheets = document.styleSheets;
+	var str = '';
+
+	for (var i = 0; i < styleSheets.length; i++) {
+		var styleSheet = styleSheets[i];
+		var rules = styleSheet.rules || styleSheet.cssRules;
+		str += "[" + styleSheet.href + "]\n";
+
+		for (var j = 0; j < rules.length; j++) {
+			var rule = rules[j];
+			str += rule.selectorText + " {\n";
+			str += rule.style.cssText + "\n";
+			str += "}\n";
+		}
+	}
+
+	return(str);
+}
