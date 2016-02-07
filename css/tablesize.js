a2b66011a0c67d12f8f6ff1515d96de99af5ab49
Hogesuke9999 <keisuke@e-nyanko.net>
2016-02-07 22:57:04 +0900

    CSSとJavascriptの試験_004

--- css/tablesize.js
+++ css/tablesize.js
@@ -9,9 +9,10 @@ function getWindowSize() {
 		document.write("横長です");
 	} else {
 		document.write("縦長です");
+		var stylesheet = document.styleSheets.item(1);
+		stylesheet.cssRules.item(0).cssText
 		css.insertRule("body.background-color: #f8dce0;");
 	};
-	document.write(getAllStyleRule());
 };
 
 function getAllStyleRule() {
